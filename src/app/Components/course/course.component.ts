22f2db645b83186e0ec846f79a9a1cfeb83c1e4a
leelanarasimha <leela.narsimha@gmail.com>
2023-07-14 00:17:04 +0530

    native shadow dom

--- src/app/Components/course/course.component.ts
+++ src/app/Components/course/course.component.ts
@@ -6,7 +6,7 @@ import { Course } from 'src/app/models/Course.model';
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css'],
-  encapsulation: ViewEncapsulation.Emulated,
+  encapsulation: ViewEncapsulation.None,
 })
 export class CourseComponent {
   @Input() course!: Course;
