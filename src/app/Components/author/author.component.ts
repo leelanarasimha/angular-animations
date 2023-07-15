4a09f39023f176c6956835380d74f98325c62df8
leelanarasimha <leela.narsimha@gmail.com>
2023-07-15 21:14:13 +0530

    ngclass class attribute

--- src/app/Components/author/author.component.ts
+++ src/app/Components/author/author.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
-  styleUrls: ['./author.component.css']
+  styleUrls: ['./author.component.css'],
 })
 export class AuthorComponent {
+  isActive: String = 'is-active';
 
+  getActiveClasses() {
+    return ['isActive'];
+  }
 }
+
+//String
+//array
