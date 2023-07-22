82e6b4ba236aa596f6071beaec12e7d5d781be44
leelanarasimha <leela.narsimha@gmail.com>
2023-07-22 16:50:03 +0530

    css transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  divClicked = false;
+  onColorChange() {
+    this.divClicked = true;
+  }
+}
