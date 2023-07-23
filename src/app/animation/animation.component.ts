165ef09f3ff7fdf636d4c58d54205e4d3f7eec26
leelanarasimha <leela.narsimha@gmail.com>
2023-07-23 18:50:26 +0530

    transition angular

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -11,6 +11,34 @@ import {
   selector: 'app-animation',
   templateUrl: './animation.component.html',
   styleUrls: ['./animation.component.css'],
-  animations: [trigger('clickedState', [])],
+  animations: [
+    trigger('clickedState', [
+      state(
+        'default',
+        style({
+          width: '50px',
+          height: '100px',
+          backgroundColor: 'orange',
+        })
+      ),
+      state(
+        'clicked',
+        style({
+          width: '100px',
+          height: '50px',
+          backgroundColor: 'blue',
+        })
+      ),
+      transition('default => clicked', animate('1s 200ms ease-in')),
+    ]),
+  ],
 })
-export class AnimationComponent {}
+export class AnimationComponent {
+  clickInfo = 'default';
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.clickInfo = 'clicked';
+    }, 3000);
+  }
+}
