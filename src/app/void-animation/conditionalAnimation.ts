feba48de9c7fee1b116d1016dc196b48852fc1c9
leelanarasimha <leela.narsimha@gmail.com>
2023-08-01 23:23:53 +0530

    wild state animations

--- src/app/void-animation/conditionalAnimation.ts
+++ src/app/void-animation/conditionalAnimation.ts
@@ -7,14 +7,28 @@ import {
 } from '@angular/animations';
 
 export const ConditionAnimation = trigger('conditionalTrigger', [
-  state('shown', style({})),
+  // transition('void => *', [
+  //   style({
+  //     opacity: 0,
+  //   }),
+  //   animate(300, style({ opacity: 1 })),
+  // ]),
 
-  transition('void => shown', [
+  // transition('* => void', [animate(300, style({ opacity: 0 }))]),
+
+  transition(':enter', [
     style({
       opacity: 0,
     }),
     animate(300, style({ opacity: 1 })),
   ]),
 
-  transition('shown => void', [animate(300, style({ opacity: 0 }))]),
+  transition(':leave', [animate(300, style({ opacity: 0 }))]),
+]);
+
+export const shrinkAnimation = trigger('shrinkAnimation', [
+  transition('* => *', [
+    animate(400, style({ width: 0 })),
+    animate(400, style({ width: '*' })),
+  ]),
 ]);
--- src/app/void-animation/void-animation.component.ts
+++ src/app/void-animation/void-animation.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { ConditionAnimation } from './conditionalAnimation';
+import { ConditionAnimation, shrinkAnimation } from './conditionalAnimation';
 
 @Component({
   selector: 'app-void-animation',
   templateUrl: './void-animation.component.html',
   styleUrls: ['./void-animation.component.css'],
-  animations: [ConditionAnimation],
+  animations: [ConditionAnimation, shrinkAnimation],
 })
 export class VoidAnimationComponent {
   showContent = false;
+  width = 400;
+  animate = false;
 
   toggleContent() {
     this.showContent = !this.showContent;
