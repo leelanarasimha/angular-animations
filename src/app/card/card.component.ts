e5fa783a29db65a5fe439ded8ed8f8a7e7b700f5
leelanarasimha <leela.narsimha@gmail.com>
2023-08-13 01:24:57 +0530

    query animation method

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,11 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  group,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,11 +15,27 @@ import { Component } from '@angular/core';
   animations: [
     trigger('cardTrigger', [
       transition(':enter', [
-        style({
-          opacity: 0,
-          transform: 'translateX(100%)',
-        }),
-        animate(300),
+        group([
+          query(
+            '.card-header',
+            style({
+              opacity: 0,
+            })
+          ),
+          style({
+            opacity: 0,
+            transform: 'translateX(100%)',
+          }),
+          animate(3000),
+          query('.card-header', [
+            style({ opacity: 0, transform: 'translateY(-100%)' }),
+            animate(3000),
+          ]),
+          query('.card-footer', [
+            style({ opacity: 0, transform: 'translateY(100%)' }),
+            animate(3000),
+          ]),
+        ]),
       ]),
       transition(':leave', [
         animate(
