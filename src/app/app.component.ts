dc50716296aa6a18c10994dd674f9b2d50ce16e7
leelanarasimha <leela.narsimha@gmail.com>
2023-08-15 21:24:41 +0530

    animation programatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AnimationBuilder, animate, style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private builder: AnimationBuilder) {}
+  animate(element: HTMLElement) {
+    const animation = this.builder.build([
+      style({ backgroundColor: 'blue', width: '100px' }),
+      animate(
+        2000,
+        style({
+          backgroundColor: 'red',
+          width: '500px',
+        })
+      ),
+      animate(300),
+    ]);
+    const player = animation.create(element);
+    player.play();
+  }
+}
